7308343d6b5415909278f17ee17a60d4e15cacb1
MirGerStudent <mirger140503@gmail.com>
2024-11-06 18:27:05 +0600

    [lab_11] add files from this lab

--- laboratory_11/index.js
+++ laboratory_11/index.js
@@ -1,6 +1,36 @@
+let current_slide = 0;
+let slides = document.getElementsByClassName("slide");
+let slide_buttons = document.getElementsByClassName("slide__button");
+
 window.onload = (() => {
-    
-    for (let i = 0; i < 7; i++) {
-                
+    for (let i = 0; i < 8; i++) {
+        slides[i].style.backgroundImage = "url(img/" + (i + 1) + ".jpg)";
+        slides[i].style.backgroundSize = "cover";
+        slide_buttons[i].addEventListener("click", () => {
+            current_slide = i;
+            updateSlide(i);
+        })
     }
+    setInterval(() => {
+    current_slide = ((current_slide % 8) + 1);
+    updateSlide(current_slide - 1);
+    }, 6000)
 })
+
+document.getElementsByClassName("prev")[0].addEventListener("click", () => {
+    current_slide = ((current_slide + 6) % 8 + 1);
+    updateSlide(current_slide - 1);
+})
+
+document.getElementsByClassName("next")[0].addEventListener("click", () => {
+    current_slide = ((current_slide % 8) + 1);
+    updateSlide(current_slide - 1);
+})
+
+const updateSlide = (slide) => {
+    document.querySelector('.images').style.transform = "translateX(" + -slide + "00%)";
+    for (let i = 0; i < slides.length; i++) {
+        slide_buttons[i].style.background = "black";
+    }
+    slide_buttons[slide].style.background = "blue";
+}
